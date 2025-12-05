66a73334a99f1cc1d5d9b9e3706e91d7c6df9e76
dreamstrata <craftaxis@gmail.com>
2025-12-05 12:02:25 -0500

    Update

--- js/site.js
+++ js/site.js
@@ -37,7 +37,7 @@ function makehtmlevents(contentbox)
         $('.schemalink').off().on('click touchstart',function(){
                 let id = $(this).attr('data')
                 console.log(id);
-                showdata('schemadata');
+                showdata(id, 'schemadata');
         });
         $('.returnlink').off().on('click touchstart',function(){
                 $('#schemadata').css('display', 'none');
